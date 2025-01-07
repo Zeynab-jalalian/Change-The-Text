449707de2b0e3afb62c6d92f8725e11f8bb0234e
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-07 21:26:44 +0330

    js code and some change

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 let text=document.querySelector('p');
 let btn=document.querySelector('button');
-let arr=['چخبر؟','همیشه بخند','چون با خنده خیلی خوشگلتری','تو واقعا منحصر به فردی','قدر خودت رو بدون']
+let arr=['میشه یه خواهشی ازت کنم؟',
+    'همیشه لبخند بزن',
+    'چون اینطوری خیلی خوشگل تر میشی',
+    'و همیشه بلند بخند',
+    'تو یه آدم منحصر به فردی',
+    'پس قدر وجودت رو بدون',
+    'چون هیچ وقت ',
+    'کسی مثل تو ',
+    'توی این جهان ',
+    'نبوده و نیست و نخواهد بود',
+    'تو بهترینی'
+]
+let index=0;
 btn.addEventListener('click',function(){
-    text.innerHTML='امیدوارم حالت خوب باشه'
+    text.innerHTML=arr[index]
+
+    index++;
+    if(index>= arr.length){
+    index=0;
+    }
 })
