e5797879567f54d4e40b26d06b4ee5b80ba58f5f
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-07 21:10:15 +0330

    make an array

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let text=document.querySelector('p');
 let btn=document.querySelector('button');
+let arr=['چخبر؟','همیشه بخند','چون با خنده خیلی خوشگلتری','تو واقعا منحصر به فردی','قدر خودت رو بدون']
 btn.addEventListener('click',function(){
     text.innerHTML='امیدوارم حالت خوب باشه'
 })
